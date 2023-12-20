062c3f3194f02297a1e36b0bbf90a9550e583c63
Scott Byers <switch120@gmail.com>
2023-12-20 08:52:18 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,9 @@
-import database from "./modules/firebase";
 import transmission from "./modules/transmission";
 
 console.clear();
 console.log("Starting Torrent-Helper Daemon ...");
 
-let promises = [];
+transmission.cleanup();
 
-let url:string;
-if (url = process.argv[2]) {
-    // push a torrent url by hand in dev mode by passing a command line argument
-    promises.push(database.ref("torrents").push().set({
-        url: url,
-        added: new Date().getTime(),
-        trnsId: null,
-        folder: process.argv[3] || null
-    }));
-
-    // just exit out if running solo (to push a magnet uri)
-    process.exit(0);
-}
-
-Promise.all(promises).then(() => {
-    // get all the torrents - find any new ones (w/o transmission ids)
-    database.ref("/torrents").orderByChild("trnsId").equalTo(null).on("value", async (snapshot) => {
-        if (!snapshot) return;
-        // add any new torrents (should only ever be one, but api supports multiple)
-        transmission.addTorrents(snapshot.val());
-    });
-
-    // run every 30s
-    setInterval(transmission.cleanup, 30*1000);
-});
+// run every 30s
+setInterval(transmission.cleanup, 30*1000);
--- src/modules/transmission.ts
+++ src/modules/transmission.ts
@@ -1,75 +1,43 @@
 const Transmission = require("transmission");
-import database from "./firebase";
 
 const tr = new Transmission({
-    host: process.env.CONTAINERIZED ? "torrentHost" : "localhost"
+  host: process.env.CONTAINERIZED ? "torrentHost" : "localhost"
 });
 
-const _getActiveTorrents = function() {
-    return new Promise((resolve, reject) => {
-        tr.get((err: any, data: any) => {
-            if (err) reject(err);
-            resolve(data.torrents);
-        });
+const _getActiveTorrents = function () {
+  console.log("Fetching active torrents ...");
+
+  return new Promise((resolve, reject) => {
+    tr.get((err: any, data: any) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      
+      console.log(`[Done] Found ${data.torrents.length} active torrents.`);
+      resolve(data.torrents);
     });
+  });
 }
 
 export default {
-    transmission: tr,
-    addTorrents: (torrents: any, options?: any) => {
-        return new Promise((resolve, reject) => {
-            if (!torrents) resolve();
+  transmission: tr,
+  getActiveTorrents: _getActiveTorrents,
+  cleanup: async function () {
+    try {
+      let torrents = (<any[]>await _getActiveTorrents());
+      torrents && torrents.forEach(torrent => {
+        if (torrent.status !== 6) return;
 
-            let promises:Promise<any>[] = [];
-            Object.keys(torrents).forEach((k:string) => {
-                promises.push(new Promise((_resolve, _reject) => {
-                    let torrent = torrents[k];
+        console.log(`Cleaning completed torrent: ${torrent}`)
 
-                    console.log("Found new Torrent; starting download: ", torrent.url);
-
-                    tr.add(torrent.url, {
-                        "download-dir": torrent.folder || "/data/uncategorized",
-                        ...options
-                    }, async (err:any, result:any) => {
-                        if (err) _reject(err);
-                            
-                        // update the torrent with the transmission id
-                        await database.ref(`/torrents/${k}`).update({
-                            trnsId: result.id
-                        });
-        
-                        _resolve(result);
-                    });
-                }));
-            });
-
-            Promise.all(promises).then(() => resolve()).catch(() => reject());
-        });
-    },
-    getActiveTorrents: _getActiveTorrents,
-    cleanup: async function() {
-        try {
-            let torrents = (<any[]>await _getActiveTorrents());
-            torrents && torrents.forEach(torrent => {
-                if (torrent.status !== 6) return;
-
-                if (process.env.TORRENT_HISTORY) {
-                    // Add this torrent to the torrentHistory, just in case it's ever needed
-                    database.ref("/torrent-history").push().set({
-                        hashString: torrent.hashString,
-                        magnetLink: torrent.magnetLink,
-                        downloadDir: torrent.downloadDir,
-                        cleaned: new Date().getTime()
-                    });
-                }
-
-                tr.remove(torrent.id, (err:any, data:any) => {
-                    console.log("Torrent cleaned: ", torrent);
-                })
-            });
-        }
-        catch (exc) {
-            console.log("Cleanup error: ", exc);
-        }
+        tr.remove(torrent.id, (err: any, data: any) => {
+          console.log("[Done] Torrent cleaned: ", torrent);
+        })
+      });
     }
+    catch (exc) {
+      console.log("Cleanup error: ", exc);
+    }
+  }
 };
